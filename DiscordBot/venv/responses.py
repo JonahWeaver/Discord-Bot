ca358e86cfac69115ade87e92538bde73803ae88
JonahWeaver <jonah.n.weaver@gmail.com>
2023-03-13 02:42:12 -0500

    Add project files.

---
+++ DiscordBot/venv/responses.py
@@ -0,0 +1,11 @@
+
+import random
+def handle_response(message) ->str:
+    p_message=message.lower()
+
+    if p_message== 'hello':
+        return 'to you too'
+    elif p_message == 'roll':
+        return str(random.randint(1,6))
+    elif p_message=='!help':
+        return "`This is a help message that you can modify.`"
